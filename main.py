7d9fd2323ed3b048ee47fb26254b94941c3ce864
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-18 20:56:30 +0200

    Stable release with functioning main.py, about to start modularization and refactoring of the complete program

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ load_dotenv()
 import os
 import enricher
 import datetime
-
+import json
+import requests
+from pymongo import MongoClient
 
 def say_hi():
     print(f"""
@@ -17,11 +19,30 @@ def say_hi():
     """)
 
 say_hi()
-datetime.datetime.today()
+print(datetime.datetime.today())
+
+def geocode(address):
+    '''
+    Use geocode api to do forward geocoding. https://geocode.xyz/api
+    '''
+    res = requests.get(f"https://geocode.xyz/{address}",params={"json":1})
+    data = res.json()
+    print(res)
+    # Return as GeoJSON -> https://geojson.org/
+    return {
+        "type":"Point",
+        "coordinates": [float(data["longt"]), float(data["latt"])]
+            }
+
+#coords = geocode('Puerta del Sol, Madrid, España')
 
 # I have to make a function to melts Geopoints into 4SQ `ll` format
 # This will be the cluster points generated from the crunchbase dataset
-ll ='40.7243,-74.0018'
+#ll = coords
+#ll = f"{ll['coordinates'][1]},{ll['coordinates'][0]}"
+ll = '40.37417177212408,-3.7015647782170413'
+
+
 
 # Define a function that makes multiple categorized requests to the API,
 # and receive each request's RESPONSES in an interesting format:
@@ -30,40 +51,113 @@ ll ='40.7243,-74.0018'
 #      - The len(array) of Input == len(dict.keys) of Output
 #      - The dict.values will be the response generated from the enricher.py module
 def cluster_request(cluster):
+    """
+    INPUT
+        - An array of Foursquare categories: https://developer.foursquare.com/docs/api-reference/venues/categories/
+    OUTPUT
+        - A Dictionary with each category as a key, and the resquests' response as its value
+    """
     results = {}
     for category in cluster:
         results[category] = enricher.getCategoryFrom4SQ(ll,category)
     return results
 
-# Call the function
+
+# IMPORTANT: ♠ This will change later to a list of categories which will be funnelled and output as GeoPoints
+# Assign the categories we will look for
+kids_points = ['daycare', 'park']
+party_points = ['convention center', 'nightlife spot']
 flight_points = ['airport', 'heliport']
+# Call the function
 flight_points_cluster = cluster_request(flight_points)
 
-# Print the outcome completely
-print(f"\n\n`flight_points_cluster`, raw variable:\n     {flight_points_cluster}")
 
-
-
-# Print unwinded data
-print(" ~ Printing unwinded data")
-# Loop the categories defined earlier as they keys to our `cluster` dictionary
+# Exporter.py module
+# ♠ OPTIMIZATION: refactor this for loop into a function
+#         It should actually take different `var_points` 
+#         and from them generate the appropiate .json 
+#         OUTPUT documents. 
+print(" ~ Exporting unwinded raw data to OUTPUT/ folder")
 for key in flight_points:
-    #print(f" ~ Printing type() of `flight_point_cluster[{key}]`: ")
+    print(f" ~ Printing key: {key}")
     response = flight_points_cluster[key]
-    #print(f"{type(response)}")
-
+    
+    #Export the places to a json
+    with open(f'OUTPUT/{key}-raw.json', 'w', encoding='utf-8') as f:
+        json.dump(response.json(),f, ensure_ascii=False, indent=4)
+               
+    #print(f"{response.json()}")
+    print(f"{type(response)}")
     #print(f" ~ Printing response.json():")
 
 
+##################################
+#################################
+#        
+#    IMPORTANT STEP !!!
+#     Formatting the output data
+#
+def GeopointFrom4SQ(venue):
     """
-    how to make the response a new json file?
+    # Turn the FourSquare venues into an ITERATOR to be sent to MongoDB
+    # Returns the formatted `blob` correctly for future use of the data
     """
-    import json
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(response.json(),f, ensure_ascii=False, indent=4)
+    loc = venue['location']
+    return {'name':venue['name'],
+            'category':venue['categories'][0]['name'], 
+            'GeoPoint':{
+                'type':'Point',       
+                'coordinates':[
+                    loc['lng'],
+                    loc['lat']
+                ]
+    }}
 
 
-    print(f"{response.json()}")
+
+# Turn the venues into an ITERATOR to be sent to MongoDB
+def venues_to_GeoPoints(venues):
+    for venue in venues:
+        print(venue)
+            # Asigning the values of the GeopointFrom4SQ() function to a new variable
+            #GeoPoint = GeopointFrom4SQ(venue)
+            #print(GeopointFrom4SQ(venue))            
+        yield GeopointFrom4SQ(venue)
+        
+
+
+# MongoClient upload
+client = MongoClient('mongodb://localhost/companies')
+db = client.get_database()
+
+
+
+# Exporter.py module
+# Output the Geopoints into `key`.json files
+
+for key in flight_points:
+    print(f" ~ Printing key: {key}")
+    print(f" ~ Printing response.json(): {response.json()}")
+    response = flight_points_cluster[key]
+    venues = response.json()['response']['venues']
+    print(list(venues_to_GeoPoints(venues)))
+    
+    #GeoPoint = GeopointFrom4SQ(venue)
+    if len(list(venues_to_GeoPoints(venues))) >0:
+        print(" ~ MONGODB - INSERTING MANY")
+        db.geo_lake.insert_many(list(venues_to_GeoPoints(venues)))
+    else:
+        print('=== empty venues')
+    #Export the places to a json
+    #ith open(f'{key}.json', 'w', encoding='utf-8') as f:c
+     #  json.dump(Geopoint,f, ensure_ascii=False, indent=4)
+               
+        
+
+    print(f"{type(response)}")
+    
+    print(f" ~ Printing response.json():")
+    #print(f"{response.json()}")
        # for venue in response:
         #    print(f" ~ Printing venue: {venue}")
            # print(venue['name'],'\n', venue['location'])
