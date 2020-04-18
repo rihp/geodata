79699d7979dbb9071869caf393e8bbdb9784a155
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-18 17:03:31 +0200

    Can take FourSquare data and return it as GeoPoints

--- main.py
+++ main.py
@@ -60,9 +60,9 @@ for key in flight_points:
     """
     import json
     with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data,f, ensure_ascii=False, indent=4)
+        json.dump(response.json(),f, ensure_ascii=False, indent=4)
 
-        
+
     print(f"{response.json()}")
        # for venue in response:
         #    print(f" ~ Printing venue: {venue}")
